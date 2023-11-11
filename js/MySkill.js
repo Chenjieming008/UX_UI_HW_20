24543127338a40f234533423bb6caf7ac564a937
Chenjieming008 <135161456+Chenjieming008@users.noreply.github.com>
2023-11-10 16:18:04 -0800

    Update

--- js/MySkill.js
+++ js/MySkill.js
@@ -1,4 +1,19 @@
+window.onscroll = function() {
+  scrollFunction();
+};
 
-$("#myID").on("click", function(){
-  console.log("You clicked on #myID");
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("back-to-top").style.display = "block";
+  } else {
+    document.getElementById("back-to-top").style.display = "none";
+  }
+}
+
+//-------- When the user clicks on the button, scroll to the top of the document--------
+
+document.getElementById('back-to-top').addEventListener('click', function(event){
+  event.preventDefault();
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
 });
